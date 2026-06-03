e5e7addf1d63b31646b52c30cacacd5934b623e1
Alexander Mohr <alexander.m.mohr@mercedes-benz.com>
2026-06-03 09:46:34 +0200

    fix: shebang for check hooks
    Signed-off-by: Alexander Mohr <alexander.m.mohr@mercedes-benz.com>

--- pre-commit-action/check-hooks-installed.py
+++ pre-commit-action/check-hooks-installed.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
 
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2026 The Contributors to Eclipse OpenSOVD (see CONTRIBUTORS)
